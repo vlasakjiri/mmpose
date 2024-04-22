5a96410e46abd6e16ffaf076f1dd4d6a035d1064
Jiří Vlasák <jirivlasak9@gmail.com>
2024-04-22 13:37:02 +0200

    Changed analyze logs

---
+++ analyze_logs.sh
@@ -0,0 +1,5 @@
+python .\tools\analysis_tools\analyze_logs.py plot_curve 
+"D:\jiriv\OneDrive\bikefit videa\runs\rtmpose-m_8xb512-700e_body8-halpe26-256x192\20240326_213121\vis_data\scalars.json" 
+"D:\jiriv\OneDrive\bikefit videa\runs\rtmpose-m_8xb512-700e_body8-halpe26-384x288\20240327_081700\vis_data\scalars.json" 
+"D:\jiriv\OneDrive\bikefit videa\runs\rtmpose-l_8xb512-700e_body8-halpe26-256x192\20240326_203805\vis_data\scalars.json" 
+--title "Training loss" --legend "RTMPose-m 256x192" "RTMPose-m 384x288" "RTMPose-l 256x192" 
